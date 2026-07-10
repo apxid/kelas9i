4166a59aa320a96566258a4cfb2290343e92f0ee
apxid <fitra.mbcell@gmail.com>
2026-07-10 09:42:44 +0700

    Create module-jejak.js

---
+++ v2/js/module-jejak.js
@@ -0,0 +1,7 @@
+console.log("Modul Jejak aktif");
+(function() {
+    // Default load saat masuk menu
+    switchSubMenu('prestasi');
+})();
+
+// Tambahkan fungsi loadDataPrestasi, loadDataInformasi, dll di sini...
